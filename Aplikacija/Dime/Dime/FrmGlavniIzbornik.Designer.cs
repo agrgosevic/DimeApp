96d6d52ba6fd864590ebfec7451d6080273402f3
Luka Gotovac <37194110+gotovac@users.noreply.github.com>
2019-06-01 15:36:34 +0200

    Izmjenjen dizajn buttona glavnog izbornika
    Izmjena u stretch

--- Aplikacija/Dime/Dime/FrmGlavniIzbornik.Designer.cs
+++ Aplikacija/Dime/Dime/FrmGlavniIzbornik.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmGlavniIzbornik));
             this.label1 = new System.Windows.Forms.Label();
             this.grpKorisnik = new System.Windows.Forms.GroupBox();
+            this.lblImeKorisnika = new System.Windows.Forms.Label();
             this.btnPomoc = new System.Windows.Forms.Button();
             this.btnIgraci = new System.Windows.Forms.Button();
             this.btnTreninzi = new System.Windows.Forms.Button();
@@ -38,7 +39,6 @@
             this.btnKorisnici = new System.Windows.Forms.Button();
             this.btnUtakmice = new System.Windows.Forms.Button();
             this.btnAktivnosti = new System.Windows.Forms.Button();
-            this.lblImeKorisnika = new System.Windows.Forms.Label();
             this.grpKorisnik.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,32 +46,41 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(10, 11);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(13, 14);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(207, 32);
+            this.label1.Size = new System.Drawing.Size(254, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Glavni izbornik";
             // 
             // grpKorisnik
             // 
             this.grpKorisnik.Controls.Add(this.lblImeKorisnika);
-            this.grpKorisnik.Location = new System.Drawing.Point(421, 11);
-            this.grpKorisnik.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.grpKorisnik.Location = new System.Drawing.Point(561, 14);
+            this.grpKorisnik.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.grpKorisnik.Name = "grpKorisnik";
-            this.grpKorisnik.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.grpKorisnik.Size = new System.Drawing.Size(138, 41);
+            this.grpKorisnik.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.grpKorisnik.Size = new System.Drawing.Size(184, 50);
             this.grpKorisnik.TabIndex = 1;
             this.grpKorisnik.TabStop = false;
             this.grpKorisnik.Text = "Ulogiran kao:";
             // 
+            // lblImeKorisnika
+            // 
+            this.lblImeKorisnika.AutoSize = true;
+            this.lblImeKorisnika.Location = new System.Drawing.Point(7, 23);
+            this.lblImeKorisnika.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblImeKorisnika.Name = "lblImeKorisnika";
+            this.lblImeKorisnika.Size = new System.Drawing.Size(91, 17);
+            this.lblImeKorisnika.TabIndex = 0;
+            this.lblImeKorisnika.Text = "Ime i prezmie";
+            // 
             // btnPomoc
             // 
             this.btnPomoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPomoc.Location = new System.Drawing.Point(563, 15);
-            this.btnPomoc.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPomoc.Location = new System.Drawing.Point(751, 18);
+            this.btnPomoc.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnPomoc.Name = "btnPomoc";
-            this.btnPomoc.Size = new System.Drawing.Size(33, 36);
+            this.btnPomoc.Size = new System.Drawing.Size(44, 44);
             this.btnPomoc.TabIndex = 2;
             this.btnPomoc.Text = "? (F1)";
             this.btnPomoc.UseVisualStyleBackColor = true;
@@ -82,10 +91,10 @@
             this.btnIgraci.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnIgraci.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIgraci.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
-            this.btnIgraci.Location = new System.Drawing.Point(15, 81);
-            this.btnIgraci.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnIgraci.Location = new System.Drawing.Point(20, 100);
+            this.btnIgraci.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnIgraci.Name = "btnIgraci";
-            this.btnIgraci.Size = new System.Drawing.Size(286, 81);
+            this.btnIgraci.Size = new System.Drawing.Size(381, 100);
             this.btnIgraci.TabIndex = 3;
             this.btnIgraci.Text = "Igrači";
             this.btnIgraci.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
@@ -94,12 +103,13 @@
             // btnTreninzi
             // 
             this.btnTreninzi.BackgroundImage = global::Dime.Properties.Resources.Treninzi;
+            this.btnTreninzi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnTreninzi.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnTreninzi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
-            this.btnTreninzi.Location = new System.Drawing.Point(310, 81);
-            this.btnTreninzi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTreninzi.Location = new System.Drawing.Point(413, 100);
+            this.btnTreninzi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnTreninzi.Name = "btnTreninzi";
-            this.btnTreninzi.Size = new System.Drawing.Size(286, 81);
+            this.btnTreninzi.Size = new System.Drawing.Size(381, 100);
             this.btnTreninzi.TabIndex = 4;
             this.btnTreninzi.Text = "Treninzi";
             this.btnTreninzi.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
@@ -108,12 +118,13 @@
             // btnStatistika
             // 
             this.btnStatistika.BackgroundImage = global::Dime.Properties.Resources.Statistika;
+            this.btnStatistika.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnStatistika.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStatistika.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
-            this.btnStatistika.Location = new System.Drawing.Point(15, 254);
-            this.btnStatistika.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnStatistika.Location = new System.Drawing.Point(20, 313);
+            this.btnStatistika.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnStatistika.Name = "btnStatistika";
-            this.btnStatistika.Size = new System.Drawing.Size(286, 81);
+            this.btnStatistika.Size = new System.Drawing.Size(381, 100);
             this.btnStatistika.TabIndex = 5;
             this.btnStatistika.Text = "Statistika";
             this.btnStatistika.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
@@ -122,12 +133,13 @@
             // btnKorisnici
             // 
             this.btnKorisnici.BackgroundImage = global::Dime.Properties.Resources.Korisnici;
+            this.btnKorisnici.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnKorisnici.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnKorisnici.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
-            this.btnKorisnici.Location = new System.Drawing.Point(15, 167);
-            this.btnKorisnici.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnKorisnici.Location = new System.Drawing.Point(20, 206);
+            this.btnKorisnici.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnKorisnici.Name = "btnKorisnici";
-            this.btnKorisnici.Size = new System.Drawing.Size(286, 81);
+            this.btnKorisnici.Size = new System.Drawing.Size(381, 100);
             this.btnKorisnici.TabIndex = 6;
             this.btnKorisnici.Text = "Korisnici";
             this.btnKorisnici.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
@@ -136,12 +148,13 @@
             // btnUtakmice
             // 
             this.btnUtakmice.BackgroundImage = global::Dime.Properties.Resources.Utakmice;
+            this.btnUtakmice.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnUtakmice.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUtakmice.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
-            this.btnUtakmice.Location = new System.Drawing.Point(310, 167);
-            this.btnUtakmice.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnUtakmice.Location = new System.Drawing.Point(413, 206);
+            this.btnUtakmice.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnUtakmice.Name = "btnUtakmice";
-            this.btnUtakmice.Size = new System.Drawing.Size(286, 81);
+            this.btnUtakmice.Size = new System.Drawing.Size(381, 100);
             this.btnUtakmice.TabIndex = 7;
             this.btnUtakmice.Text = "Utakmice";
             this.btnUtakmice.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
@@ -150,31 +163,23 @@
             // btnAktivnosti
             // 
             this.btnAktivnosti.BackgroundImage = global::Dime.Properties.Resources.Aktivnosti;
+            this.btnAktivnosti.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnAktivnosti.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAktivnosti.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
-            this.btnAktivnosti.Location = new System.Drawing.Point(310, 254);
-            this.btnAktivnosti.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAktivnosti.Location = new System.Drawing.Point(413, 313);
+            this.btnAktivnosti.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAktivnosti.Name = "btnAktivnosti";
-            this.btnAktivnosti.Size = new System.Drawing.Size(286, 81);
+            this.btnAktivnosti.Size = new System.Drawing.Size(381, 100);
             this.btnAktivnosti.TabIndex = 8;
             this.btnAktivnosti.Text = "Aktivnosti";
             this.btnAktivnosti.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
             this.btnAktivnosti.UseVisualStyleBackColor = true;
             // 
-            // lblImeKorisnika
-            // 
-            this.lblImeKorisnika.AutoSize = true;
-            this.lblImeKorisnika.Location = new System.Drawing.Point(5, 19);
-            this.lblImeKorisnika.Name = "lblImeKorisnika";
-            this.lblImeKorisnika.Size = new System.Drawing.Size(68, 13);
-            this.lblImeKorisnika.TabIndex = 0;
-            this.lblImeKorisnika.Text = "Ime i prezmie";
-            // 
             // FrmGlavniIzbornik
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(612, 352);
+            this.ClientSize = new System.Drawing.Size(816, 433);
             this.Controls.Add(this.btnAktivnosti);
             this.Controls.Add(this.btnUtakmice);
             this.Controls.Add(this.btnKorisnici);
@@ -185,6 +190,7 @@
             this.Controls.Add(this.grpKorisnik);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmGlavniIzbornik";
