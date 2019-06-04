856c54daaab91600db1ed084ad93e7da9bcb8d6e
Vinko Cvitković <37194146+vcvitkovi@users.noreply.github.com>
2019-06-04 09:54:15 +0200

    FrmDodajIgraca
    +Dodana i uređena forma za dodavanje novog igrača

---
+++ Aplikacija/Dime/Dime/Forme/Igraci/FrmDodajIgraca.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Dime.Forme.Igraci
+{
+    partial class FrmDodajIgraca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOdustani = new System.Windows.Forms.Button();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtVisina = new System.Windows.Forms.TextBox();
+            this.txtBroj = new System.Windows.Forms.TextBox();
+            this.txtTezina = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.dtpDatumRodenja = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // btnOdustani
+            // 
+            this.btnOdustani.Location = new System.Drawing.Point(204, 216);
+            this.btnOdustani.Name = "btnOdustani";
+            this.btnOdustani.Size = new System.Drawing.Size(75, 23);
+            this.btnOdustani.TabIndex = 46;
+            this.btnOdustani.Text = "Odustani";
+            this.btnOdustani.UseVisualStyleBackColor = true;
+            this.btnOdustani.Click += new System.EventHandler(this.btnOdustani_Click);
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(126, 216);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(72, 23);
+            this.btnSpremi.TabIndex = 45;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            this.btnSpremi.Click += new System.EventHandler(this.btnSpremi_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 83);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 13);
+            this.label7.TabIndex = 42;
+            this.label7.Text = "Datum rođenja";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "Broj";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 44;
+            this.label5.Text = "Težina";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 41;
+            this.label4.Text = "Visina";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 40;
+            this.label3.Text = "Prezime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 39;
+            this.label2.Text = "Ime";
+            // 
+            // txtVisina
+            // 
+            this.txtVisina.Location = new System.Drawing.Point(126, 106);
+            this.txtVisina.Name = "txtVisina";
+            this.txtVisina.Size = new System.Drawing.Size(200, 20);
+            this.txtVisina.TabIndex = 35;
+            // 
+            // txtBroj
+            // 
+            this.txtBroj.Location = new System.Drawing.Point(126, 158);
+            this.txtBroj.Name = "txtBroj";
+            this.txtBroj.Size = new System.Drawing.Size(200, 20);
+            this.txtBroj.TabIndex = 37;
+            // 
+            // txtTezina
+            // 
+            this.txtTezina.Location = new System.Drawing.Point(126, 132);
+            this.txtTezina.Name = "txtTezina";
+            this.txtTezina.Size = new System.Drawing.Size(200, 20);
+            this.txtTezina.TabIndex = 36;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(126, 54);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(200, 20);
+            this.txtPrezime.TabIndex = 34;
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(126, 28);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(200, 20);
+            this.txtIme.TabIndex = 33;
+            // 
+            // dtpDatumRodenja
+            // 
+            this.dtpDatumRodenja.Location = new System.Drawing.Point(126, 80);
+            this.dtpDatumRodenja.Name = "dtpDatumRodenja";
+            this.dtpDatumRodenja.Size = new System.Drawing.Size(200, 20);
+            this.dtpDatumRodenja.TabIndex = 47;
+            // 
+            // FrmDodajIgraca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 279);
+            this.Controls.Add(this.dtpDatumRodenja);
+            this.Controls.Add(this.btnOdustani);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtVisina);
+            this.Controls.Add(this.txtBroj);
+            this.Controls.Add(this.txtTezina);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Name = "FrmDodajIgraca";
+            this.Text = "FrmDodajIgraca";
+            this.Load += new System.EventHandler(this.FrmDodajIgraca_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOdustani;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtVisina;
+        private System.Windows.Forms.TextBox txtBroj;
+        private System.Windows.Forms.TextBox txtTezina;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.DateTimePicker dtpDatumRodenja;
+    }
+}
---
+++ Aplikacija/Dime/Dime/Forme/Igraci/FrmDodajIgraca.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dime.Forme.Igraci
+{
+    public partial class FrmDodajIgraca : Form
+    {
+        private Igrac igracZaIzmjenu;
+        public FrmDodajIgraca()
+        {
+            InitializeComponent();
+        }
+        public FrmDodajIgraca(Igrac igrac)
+        {
+            InitializeComponent();
+            igracZaIzmjenu = igrac;
+        }
+
+        private void btnSpremi_Click(object sender, EventArgs e)
+        {
+            using (var db = new DimeEntities())
+            {
+                if (igracZaIzmjenu == null)
+                {
+                    Igrac igrac = new Igrac();
+                    igrac.ime = txtIme.Text;
+                    igrac.prezime = txtPrezime.Text;
+                    igrac.datum_rodenja = dtpDatumRodenja.Value;
+                    igrac.visina = int.Parse(txtVisina.Text);
+                    igrac.tezina = int.Parse(txtTezina.Text);
+                    igrac.broj = int.Parse(txtBroj.Text);
+                }
+                else
+                {
+                    db.Igraci.Attach(igracZaIzmjenu);
+                    igracZaIzmjenu.ime = txtIme.Text;
+                    igracZaIzmjenu.prezime = txtPrezime.Text;
+                    igracZaIzmjenu.datum_rodenja = dtpDatumRodenja.Value;
+                    igracZaIzmjenu.visina = int.Parse(txtVisina.Text);
+                    igracZaIzmjenu.tezina = int.Parse(txtTezina.Text);
+                    igracZaIzmjenu.broj = int.Parse(txtBroj.Text);
+                    db.SaveChanges();
+                }
+            }
+            Close();
+        }
+
+        private void FrmDodajIgraca_Load(object sender, EventArgs e)
+        {
+            txtIme.Focus();
+            if (igracZaIzmjenu != null)
+            {
+                txtIme.Text = igracZaIzmjenu.ime;
+                txtPrezime.Text = igracZaIzmjenu.prezime;
+                //dtpDatumRodenja.Value = igracZaIzmjenu.datum_rodenja.  ;
+                txtVisina.Text = igracZaIzmjenu.visina.ToString();
+                txtTezina.Text = igracZaIzmjenu.tezina.ToString();
+                txtBroj.Text = igracZaIzmjenu.broj.ToString();
+            }
+        }
+
+        private void btnOdustani_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
